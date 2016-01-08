3132601e9c81ca79edb740b1488d7f78a8c993cf
Hans Beckerus <hans.beckerus at gmail.com>
2016-01-08 13:24:26 +0100

    Release v1.22.0

--- version.h
+++ version.h
@@ -30,7 +30,7 @@
 #define VERSION_H_
 
 #define RAR2FS_MAJOR_VER   1
-#define RAR2FS_MINOR_VER   21
-#define RAR2FS_PATCH_LVL   4
+#define RAR2FS_MINOR_VER   22
+#define RAR2FS_PATCH_LVL   0
 
 #endif
