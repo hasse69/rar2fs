hasse69/rar2fs

commit 258cd6a79a56452fb89fcbf100f1a12876e854a6

Add support for .NNN file naming scheme
Note that this patch does not only add support for .NNN style
volumes but for any naming scheme (with extension .rar) using
incremental numbering. Be aware though that starting a volume
with a non-standard name that does not resolve to a 1
(e.g. .part.001.rar) will not work reliably. Especially volume
numbers starting with 0 (e.g. .000 or .part0.rar) might fail to
extract properly. This is however not a new limitation and has
been there since day one.

This patch should resolve issue #55

Signed-off-by: Hans Beckerus <hans.beckerus at gmail.com>

rar2fs.c
@@ -182,13 +182,6 @@ struct io_handle {
                         perror("write"); \
         } while (0)
 
-#define IS_ISO(s) (!strcasecmp((s)+(strlen(s)-4), ".iso"))
-#define IS_AVI(s) (!strcasecmp((s)+(strlen(s)-4), ".avi"))
-#define IS_MKV(s) (!strcasecmp((s)+(strlen(s)-4), ".mkv"))
-#define IS_RAR(s) (!strcasecmp((s)+(strlen(s)-4), ".rar"))
-#define IS_CBR(s) (!OPT_SET(OPT_KEY_NO_EXPAND_CBR) && \
-                        !strcasecmp((s)+(strlen(s)-4), ".cbr"))
-
 /*
  * This is to the handle the workaround for the destroyed file pointer
  * position in some early libunrar5 versions when calling RARInitArchiveEx().
@@ -251,6 +244,49 @@ static const char *file_cmd[] = {
 };
 #endif
 
+/*!
+ *****************************************************************************
+ *
+ ****************************************************************************/
+static inline int is_rxx_vol(const char *name)
+{
+        size_t len = strlen(name);
+        if (name[len - 4] == '.' &&
+                        (name[len - 3] >= 'r' || name[len - 3] >= 'R') &&
+                        isdigit(name[len - 2]) && isdigit(name[len - 1])) {
+                /* This seems to be a classic .rNN rar volume file.
+                 * Let the rar header be the final judge. */
+                return 1;
+        }
+        return 0;
+}
+
+/*!
+ *****************************************************************************
+ *
+ ****************************************************************************/
+static inline int is_nnn_vol(const char *name)
+{
+        size_t len = strlen(name);
+        if (name[len - 4] == '.' &&
+                        isdigit(name[len - 3]) && isdigit(name[len - 2]) &&
+                        isdigit(name[len - 1])) {
+                /* This seems to be a .NNN rar volume file.
+                 * Let the rar header be the final judge. */
+                return 1;
+        }
+        return 0;
+}
+
+
+#define IS_ISO(s) (!strcasecmp((s)+(strlen(s)-4), ".iso"))
+#define IS_AVI(s) (!strcasecmp((s)+(strlen(s)-4), ".avi"))
+#define IS_MKV(s) (!strcasecmp((s)+(strlen(s)-4), ".mkv"))
+#define IS_RAR(s) (!strcasecmp((s)+(strlen(s)-4), ".rar"))
+#define IS_CBR(s) (!OPT_SET(OPT_KEY_NO_EXPAND_CBR) && \
+                        !strcasecmp((s)+(strlen(s)-4), ".cbr"))
+#define IS_RXX(s) (is_rxx_vol(s))
+#define IS_NNN(s) (is_nnn_vol(s))
 
 /*!
  *****************************************************************************
@@ -1531,23 +1567,6 @@ static int collect_files(const char *arch, struct dir_entry_list *list)
  *****************************************************************************
  *
  ****************************************************************************/
-static inline int is_rxx_vol(const char *name)
-{
-        size_t len = strlen(name);
-        if (name[len - 4] == '.' &&
-                        (name[len - 3] >= 'r' || name[len - 3] >= 'R') &&
-                        isdigit(name[len - 2]) && isdigit(name[len - 1])) {
-                /* This seems to be a classic .rNN rar volume file.
-                 * Let the rar header be the final judge. */
-                return 1;
-        }
-        return 0;
-}
-
-/*!
- *****************************************************************************
- *
- ****************************************************************************/
 static int get_vformat(const char *s, int t, int *l, int *p)
 {
         int len = 0;
@@ -1555,26 +1574,37 @@ static int get_vformat(const char *s, int t, int *l, int *p)
         int vol = 0;
         const size_t SLEN = strlen(s);
         if (t) {
-                int dot = 0;
-                len = SLEN - 1;
-                while (dot < 2 && len >= 0) {
-                        if (s[len--] == '.')
-                                ++dot;
+                const char *c = s + SLEN - 1;
+                while (!isdigit(*c) && c > s)
+                        c--;
+
+                const char *num = c;
+                while (isdigit(*num) && num > s)
+                        num--;
+
+                while (num > s && *num != '.') {
+                        if (isdigit(*num)) {
+                                char *dot = strchr(s, '.');
+                                if (dot != NULL && dot < num)
+                                        c = num;
+                                break;
+                        }
+                        num--;
                 }
-                if (len >= 0) {
-                        pos = len + 1;
-                        len = SLEN - pos;
-                        if (len >= 10) {
-                                if ((s[pos + 1] == 'p' || s[pos + 1] == 'P') &&
-                                    (s[pos + 2] == 'a' || s[pos + 2] == 'A') &&
-                                    (s[pos + 3] == 'r' || s[pos + 3] == 'R') &&
-                                    (s[pos + 4] == 't' || s[pos + 4] == 'T')) {
-                                        pos += 5;       /* - ".part" */
-                                        len -= 9;       /* - ".ext" */
-                                        vol = strtoul(&s[pos], NULL, 10);
-                                }
+
+                while (c != s) {
+                        if (isdigit(*c)) {
+                                --c;
+                                ++len;
+                        } else {
+                                break;
                         }
                 }
+                if (c != s)
+                        vol = strtoul(c + 1, NULL, 10);
+                else
+                        vol = 1;
+                pos =  c - s + 1;
         } else {
                 int dot = 0;
                 len = SLEN - 1;
@@ -1608,12 +1638,6 @@ static int get_vformat(const char *s, int t, int *l, int *p)
         return vol ? vol : 1;
 }
 
-#define IS_AVI(s) (!strcasecmp((s)+(strlen(s)-4), ".avi"))
-#define IS_MKV(s) (!strcasecmp((s)+(strlen(s)-4), ".mkv"))
-#define IS_RAR(s) (!strcasecmp((s)+(strlen(s)-4), ".rar"))
-#define IS_CBR(s) (!OPT_SET(OPT_KEY_NO_EXPAND_CBR) && \
-                        !strcasecmp((s)+(strlen(s)-4), ".cbr"))
-#define IS_RXX(s) (is_rxx_vol(s))
 #define IS_UNIX_MODE_(l) \
         ((l)->UnpVer >= 50 \
                 ? (l)->HostOS == HOST_UNIX \
@@ -2643,9 +2667,12 @@ static int f0(SCANDIR_ARG3 e)
         if (e->d_type != DT_UNKNOWN)
                 return (!(IS_RAR(e->d_name) && e->d_type == DT_REG) &&
                         !(IS_CBR(e->d_name) && e->d_type == DT_REG) &&
+                        !(IS_NNN(e->d_name) && e->d_type == DT_REG) &&
                         !(IS_RXX(e->d_name) && e->d_type == DT_REG));
 #endif
-        return !IS_RAR(e->d_name) && !IS_CBR(e->d_name) &&
+        return !IS_RAR(e->d_name) &&
+                        !IS_CBR(e->d_name) &&
+                        !IS_NNN(e->d_name) &&
                         !IS_RXX(e->d_name);
 }
 
@@ -2660,10 +2687,14 @@ static int f1(SCANDIR_ARG3 e)
          */
 #ifdef _DIRENT_HAVE_D_TYPE
         if (e->d_type != DT_UNKNOWN)
-                return (IS_RAR(e->d_name) || IS_CBR(e->d_name)) &&
+                return (IS_RAR(e->d_name) || 
+                                IS_CBR(e->d_name) ||
+                                IS_NNN(e->d_name)) &&
                                 e->d_type == DT_REG;
 #endif
-        return IS_RAR(e->d_name) || IS_CBR(e->d_name);
+        return IS_RAR(e->d_name) ||
+                        IS_NNN(e->d_name) ||
+                        IS_CBR(e->d_name);
 }
 
 static int f2(SCANDIR_ARG3 e)

version.h
@@ -31,6 +31,6 @@
 
 #define RAR2FS_MAJOR_VER   1
 #define RAR2FS_MINOR_VER   22
-#define RAR2FS_PATCH_LVL   7
+#define RAR2FS_PATCH_LVL   8
 
 #endif

commit d38b708fce11b046a1b2dba8ea33909bf4c59141

Fix seek length issue
commit 258cd6a79a56452fb89fcbf100f1a12876e854a6 introduced a new
problem resulting in too few volume files being searched.

Signed-off-by: Hans Beckerus <hans.beckerus at gmail.com>

optdb.c
@@ -77,10 +77,6 @@ struct opt_entry *opt_entry_p  = &opt_entry_[0];
                         OPT_(o)->m[OPT_(o)->n_elem++] = (t)strdup(s1); \
                 else \
                         OPT_(o)->m[OPT_(o)->n_elem++] = (t)strtoul(s1, NULL, 10); \
-                        if (o == OPT_KEY_SEEK_LENGTH) { \
-                                if (OPT_(o)->m[OPT_(o)->n_elem - 1]) \
-                                        OPT_(o)->m[OPT_(o)->n_elem - 1]++; \
-                        }\
         } while (0)
 
 #define CLR_OPT_(o) \

rar2fs.c
@@ -2760,7 +2760,7 @@ static void syncdir_scan(const char *dir, const char *root)
         ENTER_("%s", dir);
 
         for (f = 0; f < (sizeof(filter) / sizeof(filter[0])); f++) {
-                int vno = 0;
+                int vno = -1;
                 int i = 0;
                 int n = scandir(root, &namelist, filter[f], alphasort);
                 if (n < 0) {
@@ -2775,14 +2775,18 @@ static void syncdir_scan(const char *dir, const char *root)
                                 const size_t SLEN = strlen(namelist[i]->d_name);
                                 if (namelist[i]->d_name[SLEN - 1] == '0' &&
                                     namelist[i]->d_name[SLEN - 2] == '0') {
-                                        vno = 2;
+                                        vno = 1;
                                 } else {
                                         ++vno;
                                 }
                         } else {
-                                vno = get_vformat(namelist[i]->d_name, 1, /* new style */
-                                                        NULL, NULL);
+                                if (vno >= 0)
+                                        vno = get_vformat(namelist[i]->d_name, 1, /* new style */
+                                                          NULL, NULL);
+                                else
+                                        vno = 0;
                         }
+                        /* We always need to scan at least two volume files */
                         if (!OPT_INT(OPT_KEY_SEEK_LENGTH, 0) ||
                                         vno <= OPT_INT(OPT_KEY_SEEK_LENGTH, 0)) {
                                 char *arch;
@@ -2833,7 +2837,7 @@ static void readdir_scan(const char *dir, const char *root,
         ENTER_("%s", dir);
 
         for (f = 0; f < (sizeof(filter) / sizeof(filter[0])); f++) {
-                int vno = 0;
+                int vno = -1;
                 int i = 0;
                 int n = scandir(root, &namelist, filter[f], alphasort);
                 if (n < 0) {
@@ -2870,14 +2874,18 @@ static void readdir_scan(const char *dir, const char *root,
                                 const size_t SLEN = strlen(namelist[i]->d_name);
                                 if (namelist[i]->d_name[SLEN - 1] == '0' &&
                                     namelist[i]->d_name[SLEN - 2] == '0') {
-                                        vno = 2;
+                                        vno = 1;
                                 } else {
                                         ++vno;
                                 }
                         } else {
-                                vno = get_vformat(namelist[i]->d_name, 1, /* new style */
-                                                        NULL, NULL);
+                                if (vno >= 0)
+                                        vno = get_vformat(namelist[i]->d_name, 1, /* new style */
+                                                          NULL, NULL);
+                                else
+                                        vno = 0;
                         }
+                        /* We always need to scan at least two volume files */
                         if (!OPT_INT(OPT_KEY_SEEK_LENGTH, 0) ||
                                         vno <= OPT_INT(OPT_KEY_SEEK_LENGTH, 0)) {
                                 char *arch;
@@ -2925,7 +2933,8 @@ static void syncrar(const char *path)
         ENTER_("%s", path);
 
         int c = 0;
-        int c_end = OPT_INT(OPT_KEY_SEEK_LENGTH, 0);
+        /* We always need to scan at least two volume files */
+        int c_end = OPT_INT(OPT_KEY_SEEK_LENGTH, 0) + 1;
         struct dir_entry_list *arch_next = arch_list_root.next;
         while (arch_next) {
                 (void)listrar(path, NULL, arch_next->entry.name);
@@ -3184,7 +3193,7 @@ static int rar2_readdir(const char *path, void *buffer, fuse_fill_dir_t filler,
                 goto dump_buff;
         }
 
-        int vol = 1;
+        int vol = 0;
 
         pthread_mutex_lock(&file_access_mutex);
         dir_elem_t *entry_p = filecache_get(FH_TOPATH(fi->fh));
@@ -3217,7 +3226,7 @@ static int rar2_readdir(const char *path, void *buffer, fuse_fill_dir_t filler,
                 pthread_mutex_unlock(&file_access_mutex);
         }
 
-        if (vol == 1) {
+        if (vol == 0) {
                 dir_list_free(&dir_list);
                 return -ENOENT;
         }
@@ -3254,7 +3263,8 @@ static int rar2_readdir2(const char *path, void *buffer,
         dir_list_open(next);
 
         int c = 0;
-        int c_end = OPT_INT(OPT_KEY_SEEK_LENGTH, 0);
+        /* We always need to scan at least two volume files */
+        int c_end = OPT_INT(OPT_KEY_SEEK_LENGTH, 0) + 1;
         struct dir_entry_list *arch_next = arch_list_root.next;
         while (arch_next) {
                 (void)listrar(FH_TOPATH(fi->fh), &next, arch_next->entry.name);