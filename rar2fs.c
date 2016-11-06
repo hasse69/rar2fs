hasse69/rar2fs

commit 51f021401358f3245f3adafba29ea169625aacc5

Fix dump_buf writing to NULL and lookup_filecopy not using its cloned string. (#56)
* Only fprintf if we have a valid fp pointer in dump_buf.

* Actually use duplicated string in lookup_filecopy.

rar2fs.c
@@ -1243,9 +1243,9 @@ static void dump_buf(int seq, FILE *fp, char *buf, off_t offset, size_t size)
         size_t size_saved = size;
 
         memset(out, 0, 128);
-        fprintf(fp, "seq=%d offset: %" PRIu64 "   size: %zu   buf: %p", seq, offset, size, buf);
         size = size > 64 ? 64 : size;
         if (fp) {
+                fprintf(fp, "seq=%d offset: %" PRIu64 "   size: %zu   buf: %p", seq, offset, size, buf);
                 for (i = 0; i < size; i++) {
                         if (!i || !(i % 10)) {
                                 sprintf(tmp, "\n%016llx : ", offset + i);
@@ -2065,7 +2065,7 @@ static dir_elem_t *lookup_filecopy(const char *path, RARArchiveListEx *next,
                                 ABS_MP(mp2, (*rar_root ? rar_root : "/"), tmp);
                         } else {
                                 char *rar_dir = strdup(tmp);
-                                ABS_MP(mp2, path, basename(tmp));
+                                ABS_MP(mp2, path, basename(rar_dir));
                                 free(rar_dir);
                         }
                         e_p = filecache_get(mp2);

commit a8624e43142afc022b9fe1a6ecd78e09eeffac09

Fix size printed in function dump_buf()
The change in commit 51f021401358f3245f3adafba29ea169625aacc5
caused the size of the dumped part of the buffer to be printed
rather than the actual buffer size.

Signed-off-by: Hans Beckerus <hans.beckerus at gmail.com>

rar2fs.c
@@ -1245,7 +1245,8 @@ static void dump_buf(int seq, FILE *fp, char *buf, off_t offset, size_t size)
         memset(out, 0, 128);
         size = size > 64 ? 64 : size;
         if (fp) {
-                fprintf(fp, "seq=%d offset: %" PRIu64 "   size: %zu   buf: %p", seq, offset, size, buf);
+                fprintf(fp, "seq=%d offset: %" PRIu64 "   size: %zu   buf: %p",
+                        seq, offset, size_saved, buf);
                 for (i = 0; i < size; i++) {
                         if (!i || !(i % 10)) {
                                 sprintf(tmp, "\n%016llx : ", offset + i);