hasse69/rar2fs

commit d38b708fce11b046a1b2dba8ea33909bf4c59141

Fix seek length issue
commit 258cd6a79a56452fb89fcbf100f1a12876e854a6 introduced a new
problem resulting in too few volume files being searched.

Signed-off-by: Hans Beckerus <hans.beckerus at gmail.com>

rar2fs.c
@@ -2760,7 +2760,7 @@ static void syncdir_scan(const char *dir, const char *root)
         ENTER_("%s", dir);
 
         for (f = 0; f < (sizeof(filter) / sizeof(filter[0])); f++) {
-                int vno = 0;
+                int vno = -1;
                 int i = 0;
                 int n = scandir(root, &namelist, filter[f], alphasort);
                 if (n < 0) {
@@ -2775,14 +2775,18 @@ static void syncdir_scan(const char *dir, const char *root)
                                 const size_t SLEN = strlen(namelist[i]->d_name);
                                 if (namelist[i]->d_name[SLEN - 1] == '0' &&
                                     namelist[i]->d_name[SLEN - 2] == '0') {
-                                        vno = 2;
+                                        vno = 1;
                                 } else {
                                         ++vno;
                                 }
                         } else {
-                                vno = get_vformat(namelist[i]->d_name, 1, /* new style */
-                                                        NULL, NULL);
+                                if (vno >= 0)
+                                        vno = get_vformat(namelist[i]->d_name, 1, /* new style */
+                                                          NULL, NULL);
+                                else
+                                        vno = 0;
                         }
+                        /* We always need to scan at least two volume files */
                         if (!OPT_INT(OPT_KEY_SEEK_LENGTH, 0) ||
                                         vno <= OPT_INT(OPT_KEY_SEEK_LENGTH, 0)) {
                                 char *arch;
@@ -2833,7 +2837,7 @@ static void readdir_scan(const char *dir, const char *root,
         ENTER_("%s", dir);
 
         for (f = 0; f < (sizeof(filter) / sizeof(filter[0])); f++) {
-                int vno = 0;
+                int vno = -1;
                 int i = 0;
                 int n = scandir(root, &namelist, filter[f], alphasort);
                 if (n < 0) {
@@ -2870,14 +2874,18 @@ static void readdir_scan(const char *dir, const char *root,
                                 const size_t SLEN = strlen(namelist[i]->d_name);
                                 if (namelist[i]->d_name[SLEN - 1] == '0' &&
                                     namelist[i]->d_name[SLEN - 2] == '0') {
-                                        vno = 2;
+                                        vno = 1;
                                 } else {
                                         ++vno;
                                 }
                         } else {
-                                vno = get_vformat(namelist[i]->d_name, 1, /* new style */
-                                                        NULL, NULL);
+                                if (vno >= 0)
+                                        vno = get_vformat(namelist[i]->d_name, 1, /* new style */
+                                                          NULL, NULL);
+                                else
+                                        vno = 0;
                         }
+                        /* We always need to scan at least two volume files */
                         if (!OPT_INT(OPT_KEY_SEEK_LENGTH, 0) ||
                                         vno <= OPT_INT(OPT_KEY_SEEK_LENGTH, 0)) {
                                 char *arch;
@@ -2925,7 +2933,8 @@ static void syncrar(const char *path)
         ENTER_("%s", path);
 
         int c = 0;
-        int c_end = OPT_INT(OPT_KEY_SEEK_LENGTH, 0);
+        /* We always need to scan at least two volume files */
+        int c_end = OPT_INT(OPT_KEY_SEEK_LENGTH, 0) + 1;
         struct dir_entry_list *arch_next = arch_list_root.next;
         while (arch_next) {
                 (void)listrar(path, NULL, arch_next->entry.name);
@@ -3184,7 +3193,7 @@ static int rar2_readdir(const char *path, void *buffer, fuse_fill_dir_t filler,
                 goto dump_buff;
         }
 
-        int vol = 1;
+        int vol = 0;
 
         pthread_mutex_lock(&file_access_mutex);
         dir_elem_t *entry_p = filecache_get(FH_TOPATH(fi->fh));
@@ -3217,7 +3226,7 @@ static int rar2_readdir(const char *path, void *buffer, fuse_fill_dir_t filler,
                 pthread_mutex_unlock(&file_access_mutex);
         }
 
-        if (vol == 1) {
+        if (vol == 0) {
                 dir_list_free(&dir_list);
                 return -ENOENT;
         }
@@ -3254,7 +3263,8 @@ static int rar2_readdir2(const char *path, void *buffer,
         dir_list_open(next);
 
         int c = 0;
-        int c_end = OPT_INT(OPT_KEY_SEEK_LENGTH, 0);
+        /* We always need to scan at least two volume files */
+        int c_end = OPT_INT(OPT_KEY_SEEK_LENGTH, 0) + 1;
         struct dir_entry_list *arch_next = arch_list_root.next;
         while (arch_next) {
                 (void)listrar(FH_TOPATH(fi->fh), &next, arch_next->entry.name);

optdb.c
@@ -77,10 +77,6 @@ struct opt_entry *opt_entry_p  = &opt_entry_[0];
                         OPT_(o)->m[OPT_(o)->n_elem++] = (t)strdup(s1); \
                 else \
                         OPT_(o)->m[OPT_(o)->n_elem++] = (t)strtoul(s1, NULL, 10); \
-                        if (o == OPT_KEY_SEEK_LENGTH) { \
-                                if (OPT_(o)->m[OPT_(o)->n_elem - 1]) \
-                                        OPT_(o)->m[OPT_(o)->n_elem - 1]++; \
-                        }\
         } while (0)
 
 #define CLR_OPT_(o) \

commit cd23ba40b9952677466e34ea7ad40f9dc5c503cc

Fix seek length issue for archive mounts
This patch fixes an issue with seek length introduced by
commit d38b708fce11b046a1b2dba8ea33909bf4c59141 and reported
in issue #61.

rar2fs.c
@@ -2951,11 +2951,12 @@ static void syncrar(const char *path)
 
         int c = 0;
         /* We always need to scan at least two volume files */
-        int c_end = OPT_INT(OPT_KEY_SEEK_LENGTH, 0) + 1;
+        int c_end = OPT_INT(OPT_KEY_SEEK_LENGTH, 0);
+        c_end = c_end ? c_end + 1 : c_end;
         struct dir_entry_list *arch_next = arch_list_root.next;
         while (arch_next) {
                 (void)listrar(path, NULL, arch_next->entry.name);
-                if (c_end && ++c == c_end)
+                if (++c == c_end)
                         break;
                 arch_next = arch_next->next;
         }
@@ -3281,11 +3282,12 @@ static int rar2_readdir2(const char *path, void *buffer,
 
         int c = 0;
         /* We always need to scan at least two volume files */
-        int c_end = OPT_INT(OPT_KEY_SEEK_LENGTH, 0) + 1;
+        int c_end = OPT_INT(OPT_KEY_SEEK_LENGTH, 0);
+        c_end = c_end ? c_end + 1 : c_end;
         struct dir_entry_list *arch_next = arch_list_root.next;
         while (arch_next) {
                 (void)listrar(FH_TOPATH(fi->fh), &next, arch_next->entry.name);
-                if (c_end && ++c == c_end)
+                if (++c == c_end)
                         break;
                 arch_next = arch_next->next;
         }