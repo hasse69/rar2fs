4d21278ddff8e10fafb72d6a9fa90912fadb1e78
Hans Beckerus <hans.beckerus at gmail.com>
2017-10-24 20:33:38 +0200

    Release v1.26.0
    Signed-off-by: Hans Beckerus <hans.beckerus at gmail.com>

--- version.h
+++ version.h
@@ -30,7 +30,7 @@
 #define VERSION_H_
 
 #define RAR2FS_MAJOR_VER   1
-#define RAR2FS_MINOR_VER   25
-#define RAR2FS_PATCH_LVL   2
+#define RAR2FS_MINOR_VER   26
+#define RAR2FS_PATCH_LVL   0
 
 #endif
