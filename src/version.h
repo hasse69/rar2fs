b6d1d42952d8ec7153bc1c859de88b288cf6b5d6
Hans Beckerus <hans.beckerus at gmail.com>
2019-11-06 20:11:55 +0100

    Release v1.28.0
    Signed-off-by: Hans Beckerus <hans.beckerus at gmail.com>

--- src/version.h
+++ src/version.h
@@ -30,7 +30,7 @@
 #define VERSION_H_
 
 #define RAR2FS_MAJOR_VER   1
-#define RAR2FS_MINOR_VER   27
-#define RAR2FS_PATCH_LVL   2
+#define RAR2FS_MINOR_VER   28
+#define RAR2FS_PATCH_LVL   0
 
 #endif
