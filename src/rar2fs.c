hasse69/rar2fs

commit 96cf4b949960e7c88c07caefbf76a8f30a04c5a1

Improve speed when listing archives with many directories
The population of the directory cache is "lazy", which basically means
it is not populated until a directory is queried by e.g. a readdir() call.
The effect of this is that the entire archive is scanned multiple times,
ones for each directory query. Become more aggressive in the population of
the directory cache. Basically making sure each directory in an archive is
added to the cache at first scan, even if it was not specifically the
subject of the scan itself.
This patch addresses part two of the problem reported in issue #124.

Signed-off-by: Hans Beckerus <hans.beckerus at gmail.com>

src/rar2fs.c
@@ -2410,6 +2410,37 @@ static void __listrar_tocache_forcedir(struct filecache_entry *entry_p,
  *****************************************************************************
  *
  ****************************************************************************/
+static inline void __listrar_cachedir(const char *mp)
+{
+        if (!dircache_get(mp))
+		(void)dircache_alloc(mp);
+}
+
+/*!
+ *****************************************************************************
+ *
+ ****************************************************************************/
+static void __listrar_cachedirentry(const char *mp)
+{
+        char *safe_path = strdup(mp);
+        char *tmp = safe_path;
+        safe_path = __gnu_dirname(safe_path);
+        if (CHRCMP(safe_path, '/')) {
+                struct dircache_entry *dce = dircache_get(safe_path);
+                if (dce) {
+                        char *tmp = strdup(mp);
+                        dir_entry_add(&dce->dir_entry_list, basename(tmp),
+                                      NULL, DIR_E_RAR);
+                        free(tmp);
+                }
+        }
+        free(tmp);
+}
+
+/*!
+ *****************************************************************************
+ *
+ ****************************************************************************/
 static int listrar(const char *path, struct dir_entry_list **buffer,
                 const char *arch, char **first_arch, int *final)
 {
@@ -2444,7 +2475,7 @@ static int listrar(const char *path, struct dir_entry_list **buffer,
         tmp1 = rar_root;
         rar_root += strlen(OPT_STR2(OPT_KEY_SRC, 0));
         int is_root_path = (!strcmp(rar_root, path) || !CHRCMP(path, '/'));
-	int ret = 0;
+        int ret = 0;
         RARArchiveDataEx *arc = NULL;
 
         if (*first_arch == NULL) {
@@ -2502,6 +2533,7 @@ static int listrar(const char *path, struct dir_entry_list **buffer,
                                         entry_p = filecache_alloc(mp2);
                                         __listrar_tocache_forcedir(entry_p, arc,
                                                         safe_path, *first_arch, &d);
+                                        __listrar_cachedir(mp2);
                                 }
                                 free(mp2);
                         }
@@ -2546,6 +2578,9 @@ static int listrar(const char *path, struct dir_entry_list **buffer,
 cache_hit:
                 pthread_rwlock_unlock(&file_access_lock);
                 __add_filler(path, buffer, mp);
+                if (IS_RAR_DIR(&arc->hdr))
+                        __listrar_cachedir(mp);
+                __listrar_cachedirentry(mp);
                 free(mp);
         }
 
@@ -3237,38 +3272,7 @@ static int rar2_readdir(const char *path, void *buffer, fuse_fill_dir_t filler,
 
         pthread_rwlock_rdlock(&file_access_lock);
         struct filecache_entry *entry2_p = filecache_get(path);
-again:
-        if (entry2_p) {
-                char *tmp = strdup(entry2_p->rar_p);
-                int multipart = entry2_p->flags.multipart;
-                short vtype = entry2_p->vtype;
-                pthread_rwlock_unlock(&file_access_lock);
-                if (multipart) {
-                        int vol = 0;
-                        int final = 0;
-                        int vol_end = get_seek_length(entry2_p->rar_p);
-                        vol_end = vol_end ? vol_end + 1 : vol_end;
-                        printd(3, "Search for local directory in %s\n", tmp);
-                        while (!listrar(path, &next2, tmp, &tmp, &final)) {
-                                if ((++vol == vol_end) || final) {
-                                        free(tmp);
-                                        goto dump_buff;
-                                }
-                                RARNextVolumeName(tmp, !vtype);
-                                printd(3, "Search for local directory in %s\n", tmp);
-                        }
-                } else {
-                        if (tmp) {
-                                printd(3, "Search for local directory in %s\n", tmp);
-                                if (!listrar(path, &next2, tmp, &tmp, NULL)) {
-                                        free(tmp);
-                                        goto dump_buff;
-                                }
-                        }
-                }
-
-                free(tmp);
-        } else {
+        if (!entry2_p) {
                 /* It is possible but not very likely that we end up here
                  * due to that the cache has not yet been populated.
                  * Scan through the entire file path to force a cache update.
@@ -3287,7 +3291,7 @@ again:
                 pthread_rwlock_rdlock(&file_access_lock);
                 entry2_p = filecache_get(path);
                 if (entry2_p)
-                        goto again;
+                        goto dump_buff;
                 pthread_rwlock_unlock(&file_access_lock);
 		if (dp == NULL)
 		        goto no_entry;
@@ -3982,7 +3986,7 @@ static pthread_cond_t warmup_cond = PTHREAD_COND_INITIALIZER;
  ****************************************************************************/
 static void *walkpath_task(void *data)
 {
-	pthread_detach(pthread_self());
+        pthread_detach(pthread_self());
 
         syncdir(data);
         free(data);

src/dirlist.c
@@ -122,12 +122,17 @@ void dir_list_free(struct dir_entry_list *root)
 struct dir_entry_list *dir_entry_add(struct dir_entry_list *l, const char *key,
                 struct stat *st, int type)
 {
+        struct dir_entry_list *tmp = l;
         uint32_t hash = get_hash(key, 0);
-        if (l->entry.head_flag != DIR_LIST_HEAD_) {
+
+        while (l) {
                 if (hash == l->entry.hash)
                         if (!strcmp(key, l->entry.name))
                                 return l;
+                tmp = l;
+                l = l->next;
         }
+        l = tmp;
         l->next = malloc(sizeof(struct dir_entry_list));
         if (l->next) {
                 l = l->next;

commit a67879c7014b93f1e9c8f28cf97b65b14c9c853e

Fix regression for issue #114
Commit 96cf4b949960e7c88c07caefbf76a8f30a04c5a1 caused a regression
for the solution addressing issue #114.

Signed-off-by: Hans Beckerus <hans.beckerus at gmail.com>

src/rar2fs.c
@@ -3270,30 +3270,27 @@ static int rar2_readdir(const char *path, void *buffer, fuse_fill_dir_t filler,
         if (entry_p)
                 goto dump_buff;
 
-        pthread_rwlock_rdlock(&file_access_lock);
-        struct filecache_entry *entry2_p = filecache_get(path);
-        pthread_rwlock_unlock(&file_access_lock);
-        if (!entry2_p) {
-                /* It is possible but not very likely that we end up here
-                 * due to that the cache has not yet been populated.
-                 * Scan through the entire file path to force a cache update.
-                 * This is the same action as required for a cache miss in
-                 * getattr(). */
-                char *safe_path = strdup(path);
-                char *tmp = safe_path;
-                while (1) {
-                        safe_path = __gnu_dirname(safe_path);
-                        syncdir(safe_path);
-                        if (!strcmp(safe_path, "/"))
-                                break;
-                }
-                free(tmp);
-                pthread_rwlock_rdlock(&file_access_lock);
-                entry2_p = filecache_get(path);
-                pthread_rwlock_unlock(&file_access_lock);
-                if (!entry2_p && dp == NULL)
-                        goto no_entry;
+        /* It is possible but not very likely that we end up here
+         * due to that the cache has not yet been populated.
+         * Scan through the entire file path to force a cache update.
+         * This is the same action as required for a cache miss in
+         * getattr(). */
+        char *safe_path = strdup(path);
+        char *tmp = safe_path;
+        while (1) {
+                safe_path = __gnu_dirname(safe_path);
+                syncdir(safe_path);
+                if (!strcmp(safe_path, "/"))
+                        break;
         }
+        free(tmp);
+        pthread_rwlock_rdlock(&dir_access_lock);
+        entry_p = dircache_get(path);
+        if (entry_p) {
+                free(dir_list2);
+                dir_list2 = dir_list_dup(&entry_p->dir_entry_list);
+        }
+        pthread_rwlock_unlock(&dir_access_lock);
 
 dump_buff:
 
@@ -3332,17 +3329,6 @@ dump_buff_nocache:
         free(dir_list2);
 
         return 0;
-
-no_entry:
-
-        dir_list_free(&dir_list);
-        if (entry_p) {
-                dir_list_free(dir_list2);
-                free(dir_list2);
-        }
-
-        return -ENOENT;
-
 }
 
 /*!